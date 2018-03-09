14450d8af93abf2b91a48333691fbbecb06dd3bc
molvik <oystein.m@hotmail.com>
2018-03-09 11:26:04 +0100

    Oppgave 1 done

--- gpio/main.c
+++ gpio/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
+#include <stdio.h>
 
-#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
+#define GPIO ((NRF_GPIO_REGS*)0x50000000)
 
 typedef struct {
 	volatile uint32_t RESERVED0[321];
@@ -11,7 +12,7 @@ typedef struct {
 	volatile uint32_t DIR;
 	volatile uint32_t DIRSET;
 	volatile uint32_t DIRCLR;
-	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
+	volatile uint32_t RESERVED1[120];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
@@ -23,14 +24,31 @@ int main(){
 	}
 
 	// Configure buttons
-	GPIO->PIN_CNF[__BUTTON_A_PIN__] = 0;
-	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
+	GPIO->PIN_CNF[17] = 0;
+	GPIO->PIN_CNF[26] = 0;
 
 	int sleep = 0;
+
+	//int button_A = 0x744;
+	int button_A = (1 << 17);
+	int button_B = (1 << 26);
+
 	while(1){
 
 		/* Check if button B is pressed;
 		 * turn on LED matrix if it is. */
+		if (!(GPIO->IN & button_A)) {
+			GPIO->OUTSET = (1 << 13);
+			GPIO->OUTSET = (1 << 14);
+			GPIO->OUTSET = (1 << 15);
+		}
+
+		if (!(GPIO->IN & button_B)) {
+			GPIO->OUTCLR = (1 << 13);
+			GPIO->OUTCLR = (1 << 14);
+			GPIO->OUTCLR = (1 << 15);
+		}
+		
 
 		/* Check if button A is pressed;
 		 * turn off LED matrix if it is. */
